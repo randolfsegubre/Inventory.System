e4d482f30956730f832cb149d9bbb436ee4e9669
Randolf Segubre <rsegubre@gmail.com>
2020-06-22 18:20:02 +0800

    delete app data

---
+++ Inventory.System/Inventory.System.Data/Contexts/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Inventory.System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Inventory.System.Data.Contexts
+{
+    public class MyDataContext : DbContext, IDisposable
+    {
+
+        DbSet<Users> Users { get; set; }
+        DbSet<UserDetails> UserDetails {get;set;}
+
+        public MyDataContext(DbContextOptions options) : base(options) 
+        {
+        }
+    }
+}
---
+++ Inventory.System/Inventory.System.Data/Entity/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Inventory.System.Data.Entity
+{
+    public class Users
+    {
+        [Key]
+        public int User_Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public UserDetails Details { get; set; }
+    }
+
+    public class UserDetails
+    {
+        [Key]
+        public int UserDetails_Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public char Gender { get; set; }
+    }
+}
