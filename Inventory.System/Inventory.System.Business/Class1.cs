60e3460407bad00c3765d75a1979485b025153e0
Randolf Segubre <rsegubre@gmail.com>
2021-05-01 00:10:06 +0800

    clear all project files

--- Inventory.System/Inventory.System.Business/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Inventory.System.Business
-{
-    public class Class1
-    {
-    }
-}
--- Inventory.System/Inventory.System.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Inventory.System.Data
-{
-    public class Class1
-    {
-    }
-}
